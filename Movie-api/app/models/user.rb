700b6dbbc63423110ef661c7e134dca1e605b284
dp_chua <dp_chua@hotmail.com>
2017-03-02 12:50:19 -0500

    added gems

--- Movie-api/app/models/user.rb
+++ Movie-api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews, dependent: :destroy
-  has_many :movies, through: :reviews 
+  has_many :movies, through: :reviews
   validates :name, presence: true, uniqueness: true
 
 end
