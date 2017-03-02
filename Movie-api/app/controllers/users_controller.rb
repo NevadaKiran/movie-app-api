9e1e3670a7130e734bd8f8335fe35b0754e149b2
dp_chua <dp_chua@hotmail.com>
2017-03-02 14:38:15 -0500

    token

--- Movie-api/app/controllers/users_controller.rb
+++ Movie-api/app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authorize, except: [:login, :create]
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 200, message: "ok"}
+    else
+      render json: {status: 422, user: user.errors}
+    end
+  end
 
   def login
     user = User.find_by(email: params[:user][:email])
-
     if user && user.authenticate(params[:user][:password])
-      render json: {status: 201, user: user}
+      token = token(user.id, user.email)
+
+      render json: {status: 201, user: user, token: token}
     else
       render json: {status: 401, message: "unauthorized"}
     end
@@ -15,16 +26,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: User.find(params[:id])
-  end
-
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: {status: 200, message: "ok"}
-    else
-      render json: {status: 422, user: user.errors}
-    end
+    render json: {status: 200, user: current_user}
   end
 
   def update
@@ -44,7 +46,24 @@ class UsersController < ApplicationController
 
   private
 
+  def token(id, email)
+    JWT.encode(payload(id, email), 'someawesomesecret', 'HS256')
+  end
+
+  def payload(id, email)
+    {
+      exp: (Time.now + 30.minutes).to_i,
+      iat: Time.now.to_i,
+      iss: 'wdir-matey',
+      user: {
+        id: id,
+        email: email
+      }
+    }
+  end
+
   def user_params
       params.require(:user).permit(:name, :email, :password)
   end
+
 end
