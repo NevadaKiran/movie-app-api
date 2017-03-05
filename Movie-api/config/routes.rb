7b3b717ee35f6d083f6286a7aad64190d8444e35
KiranWork <kiran8654@yahoo.com>
2017-03-04 23:30:22 -0800

    route.rb change

--- Movie-api/config/routes.rb
+++ Movie-api/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show, :destroy] do
-    resources :movies
-    resources :reviews
+
     collection do
       post '/login', to: 'users#login'
     end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :movies
+  resources :reviews
 end
